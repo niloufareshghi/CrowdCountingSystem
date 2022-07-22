3c70321b8aba22dd181427e07f849ad711f07f71
Niloufar Eshghi <niloofareshghi87@gmail.com>
2022-07-22 11:40:48 -0400

    connect webpage to crowd counting model

--- CrowdCountingModels/main.py
+++ CrowdCountingModels/main.py
@@ -1,16 +1,12 @@
 import models
-from MSFANet.models import M_SFANet_UCF_QNRF
 import torch
 
 # test image
-path = "./MSFANet/images/densecrowd1.jpg"
+path = ""
 
 print("Which model do you wish to use?")
 print("For MSFANET enter m, for CAN enter c, for P2P enter p")
 model = input()
 if model == 'm':
-    models.msfanet(path, M_SFANet_UCF_QNRF)
-elif model == 'c':
-    models.can(path)
-elif model == 'p':
-    models.p2pnet()
+    models.msfanet(path)
+
--- CrowdCountingModels/models.py
+++ CrowdCountingModels/models.py
@@ -3,14 +3,14 @@ from PIL import Image
 import numpy as np
 import torch
 from torchvision import transforms
+from CrowdCountingModels.MSFANet.models import M_SFANet_UCF_QNRF
 
 transform = transforms.Compose([transforms.ToTensor(),
                                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
                                 ])
 
 
-def msfanet(path, pretrained):
-    # Simple preprocessing.
+def msfanet(path):
 
     # An example image
     # path = "./MSFANet/images/densecrowd1.jpg"
@@ -21,14 +21,14 @@ def msfanet(path, pretrained):
     img = cv2.resize(np.array(img), (width, height), cv2.INTER_CUBIC)
     img = transform(Image.fromarray(img))[None, :]
 
-    model = pretrained.Model()
+    model = M_SFANet_UCF_QNRF.Model()
     # Weights are stored in the Google drive link.
     # The model are originally trained on a GPU but, we can also test it on a CPU.
-    model.load_state_dict(torch.load("./MSFANet/Paper's_weights_UCF_QNRF/best_M-SFANet__UCF_QNRF.pth",
+    model.load_state_dict(torch.load("CrowdCountingModels/MSFANet/Paper's_weights_UCF_QNRF/best_M-SFANet__UCF_QNRF.pth",
                                      map_location=torch.device('cpu')))
 
     # Evaluation mode
     model.eval()
     density_map = model(img)
 
-    print(torch.sum(density_map).item())  # Est. count = 147.14852905273438
+    return torch.sum(density_map).item()  # Est. count = 147.14852905273438
