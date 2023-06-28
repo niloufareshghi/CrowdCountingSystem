df434afc7aae91affc4d24b954a4787090f67b54
Niloufar Eshghi <niloofareshghi87@gmail.com>
2023-06-28 09:59:21 -0400

    update django models

--- counting/models.py
+++ counting/models.py
@@ -3,11 +3,21 @@ from django.db.models import FileField, DateTimeField
 
 
 class Document(models.Model):
-    description = models.CharField(max_length=255, blank=True)
-    document = models.FileField(upload_to='documents/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
+    document = models.FileField()
+    uploaded_at = models.DateTimeField()
+    method = models.CharField(max_length=10, default="")
 
 
-class FileModel(models.Model):
-    file = FileField(upload_to='uploaded_files/', default='')
-    created = DateTimeField(auto_now=True)
+class Submission(models.Model):
+    file = FileField(upload_to="uploaded_files/", default="")
+    model = models.CharField(max_length=10, default="")
+    SUBMITTED = 0
+    RUNNING = 1
+    SUCCESS = 2
+    FAILED = 3
+    STATUS_CHOICES = [(0, "Submitted"), (1, "Running"), (2, "Success"), (3, "Failed")]
+    status = models.IntegerField(choices=STATUS_CHOICES, default=SUBMITTED)
+    count = models.IntegerField(default=0)
+    predict = models.CharField(default="", max_length=50)
+    message = models.CharField(default="", max_length=100)
+    model_name_desc = models.CharField(default="", max_length=250)
