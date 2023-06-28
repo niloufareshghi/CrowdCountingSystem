839a494bb8bfa08af2710e78dc9d417daa8ec179
Niloufar Eshghi <niloofareshghi87@gmail.com>
2023-06-28 09:46:06 -0400

    Update urls.py

--- counting/urls.py
+++ counting/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),
-    path('', views.model_form_upload, name='upload'),
+    path('', views.index, name='index'),
+    path("home", views.home, name='home'),
+    path("upload", views.upload, name="upload"),
+    path("result/<int:submission_id>", views.result, name="result"),
 ]
